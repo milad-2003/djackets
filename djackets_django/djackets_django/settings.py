a9f7a797cdd0f39da259a8abf4d73df9921dd4af
Milad Valizadeh <miladvalizadeh2003@gmail.com>
2024-12-11 01:16:07 +0330

    Create django "product" app and the related models

--- djackets_django/djackets_django/settings.py
+++ djackets_django/djackets_django/settings.py
@@ -37,10 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'rest_framework',
     'rest_framework.authtoken',
     'corsheaders',
     'djoser',
+
+    'product',
 ]
 
 CORS_ALLOWED_ORIGINS = [
@@ -126,6 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
