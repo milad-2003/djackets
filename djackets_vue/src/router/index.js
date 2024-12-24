e1ce7ca40f48c91e05a794af788604bb20ccca86
Milad Valizadeh <miladvalizadeh2003@gmail.com>
2024-12-25 00:55:21 +0330

    Added search functionality

--- djackets_vue/src/router/index.js
+++ djackets_vue/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
+import Search from '../views/Search.vue'
 import Product from '../views/Product.vue'
 import Category from '../views/Category.vue'
 
@@ -17,6 +18,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/search',
+    name: 'Search',
+    component: Search
   },
   {
     path: '/:category_slug/:product_slug',
