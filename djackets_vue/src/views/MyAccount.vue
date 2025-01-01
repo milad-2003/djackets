d71844d3ae8767e2e79e61682216ee2e33ab20a7
Milad Valizadeh <miladvalizadeh2003@gmail.com>
2025-01-01 12:23:38 +0330

    Showing "My orders" in "My account" page

--- djackets_vue/src/views/MyAccount.vue
+++ djackets_vue/src/views/MyAccount.vue
@@ -7,6 +7,17 @@
             <div class="column is-12">
                 <button @click="logout()" class="button is-danger">Log out</button>
             </div>
+
+            <hr>
+
+            <div class="column is-12">
+                <h2 class="subtitle">My orders</h2>
+
+                <OrderSummary
+                v-for="order in orders"
+                v-bind:key="order.id"
+                v-bind:order="order" />
+            </div>
         </div>
     </div>
 </template>
@@ -14,8 +25,23 @@
 <script>
 import axios from 'axios'
 
+import OrderSummary from '@/components/OrderSummary.vue';
+
 export default {
     name: 'MyAccount',
+    components: {
+        OrderSummary
+    },
+    data() {
+        return {
+            orders: []
+        }
+    },
+    mounted() {
+        document.title = 'My account | Djackets'
+
+        this.getMyOrders()
+    },
     methods: {
         logout() {
             axios.defaults.headers.common['Authorization'] = ""
@@ -27,11 +53,22 @@ export default {
             this.$store.commit('removeToken')
 
             this.$router.push('/')
+        },
+        async getMyOrders() {
+            this.$store.commit('setIsLoading', true)
+
+            await axios
+                    .get('/api/v1/orders')
+                    .then(response => {
+                        this.orders = response.data
+                    })
+                    .catch(error => {
+                        console.log(error)
+                    })
+
+            this.$store.commit('setIsLoading', false)
         }
     },
-    mounted() {
-        document.title = 'My account | Djackets'
-    }
 }
 
 </script>
